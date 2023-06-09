ce9df624a5af5a05d7b78ff7ef554a57de2ea50d
Lukas Valskis <gtlukis6@gmail.com>
2023-06-09 18:42:15 +0300

    Updated UserInput class

--- src/main/java/lt/viko/moviesearch/model/UserInput.java
+++ src/main/java/lt/viko/moviesearch/model/UserInput.java
@@ -1,4 +1,38 @@
 package lt.viko.moviesearch.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class UserInput {
+
+    @JsonProperty("movieTitle")
+    private String movieTitle;
+
+    @JsonProperty("actorName")
+    private String actorName;
+
+    //Default constructor required by Jackson
+    public UserInput() {
+    }
+
+    //Constructor
+    public UserInput(String movieTitle, String actorName) {
+        this.movieTitle = movieTitle;
+        this.actorName = actorName;
+    }
+
+    public String getMovieTitle() {
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String movieTitle) {
+        this.movieTitle = movieTitle;
+    }
+
+    public String getActorName() {
+        return actorName;
+    }
+
+    public void setActorName(String actorName) {
+        this.actorName = actorName;
+    }
 }
